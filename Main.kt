6555193b8a42ab6f84b4b9983a170b6840646f94
Faith-Ndungu <wanjirufaithndunguu@gmail.com>
2024-02-07 13:09:46 +0300

    add one file

---
+++ Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var name = "Faith Ndung'u"
+    println(name)
+    var age = 20
+    println(age)
+    var number = +25423433511
+    println(number)
+    var weight = 60
+    println(weight)
+    var nationality = "Kenya"
+    println(nationality)
+    val citizen = true
+    val notcitizen = false
+    println(citizen)
+}
+
+
